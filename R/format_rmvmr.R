e9dea1ae473fd6fb3d3770cb983941367e743282
Tom Palmer <remlapmot@hotmail.com>
2021-01-18 20:08:26 +0000

    delete trailing spaces

--- R/format_rmvmr.R
+++ R/format_rmvmr.R
@@ -30,74 +30,74 @@
 #numbers for the instruments.
 
 format_rmvmr<-function(BXGs,BYG,seBXGs,seBYG,RSID){
-  
+
   #If no instrument-identification vector is provided, a set of placeholder values
   #is produced. A warning is also given to indicate no values were provided
-  
+
   if(missing(RSID)) {
     RSID<-seq(from=1,to=length(BYG),by=1)
     warning("Missing SNP IDs; Generating placeholders")
   }
-  
+
   #This loop names each column of instrument-exposure associations in the order
   #they appear in the provided matrix, labeling each exposure with an X and
   #subsequent index number. The first exposure provided is labelled betaX1.
-  
+
   BXGs<-data.frame(BXGs)
   seBXGs<-data.frame(seBXGs)
   BYG<-data.frame(BYG)
   seBYG<-data.frame(seBYG)
   RSID<-data.frame(RSID)
-  
+
   for(i in 1:ncol(BXGs)){
     names(BXGs)[i]<-paste0("betaX",i,collapse=",")
   }
-  
+
   #This loop names each column of instrument-exposure standard errors in the order
   #they appear in the provided matrix, labeling each exposure with an X and
   #subsequent index number. The standard error for the first exposure provided
   #is labelled sebetaX1.
-  
+
   for(i in 1:ncol(seBXGs)){
     names(seBXGs)[i]<-paste0("sebetaX",i,collapse=",")
   }
-  
+
   # A dataframe containing all the necessary information for performing
   #multivariable MR is created, placing the variables in a specific order.
-  
+
   dat<-cbind(RSID,BYG,seBYG,BXGs,seBXGs)
-  
+
   # The columns of the dataframe are renamed so as to be interpretable in
   #subsequent functions.
-  
+
   for(i in 1:ncol(dat)){
-    
+
     if(i>1){
       dat[,i]<-as.numeric(dat[,i])
-      
+
     }
-    
+
   }
-  
+
   names(dat)<-c("SNP","betaYG","sebetaYG",names(BXGs),names(seBXGs))
-  
+
   #Determine the number of exposures
   exp.number<-length(names(dat)[-c(1,2,3)])/2
-  
+
   for(i in 4:(4+exp.number-1)){
     for(j in 1:length(dat[,1])){
-    
+
     if(dat[j,i] == 0){
       dat[j,i] <- 0.00001
       }
     }
-  }  
-  
+  }
+
   #Append rmvmr_format class to output data frame
   class(dat) <- append(class(dat),
                         "rmvmr_format")
-  
+
   # The function returns the formatted dataframe
-  
+
   return(dat)
 }
