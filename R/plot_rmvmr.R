5e047745494f3e926f754f7cb4b0c9b595e6dc05
Tom Palmer <remlapmot@hotmail.com>
2021-01-18 20:30:05 +0000

    formatting

--- R/plot_rmvmr.R
+++ R/plot_rmvmr.R
@@ -15,7 +15,7 @@
 #'}
 #'
 #' @author Wes Spiller; Eleanor Sanderson; Jack Bowden.
-#'@references Spiller, W., et al., Estimating and visualising multivariable Mendelian randomization analyses within a radial framework. Forthcoming.
+#' @references Spiller, W., et al., Estimating and visualising multivariable Mendelian randomization analyses within a radial framework. Forthcoming.
 #' @export
 #' @examples
 #'
